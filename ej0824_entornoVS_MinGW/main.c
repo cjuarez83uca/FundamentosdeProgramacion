88fc6223d3dc7c5472429fc573b7610c4dcc3fa8
Carlos Juárez <cjuarez@uca.edu.sv>
2022-08-24 15:32:17 -0600

    ejercicio 24082022

---
+++ ej0824_entornoVS_MinGW/main.c
@@ -0,0 +1,9 @@
+// libreria de entrada y salida
+#include <stdio.h>
+
+int main(int argc,char *argv[])
+{
+    printf("\n\n");
+    printf("hola companieros de Fundamentos (otra vez)\n");
+    printf("\n\n");
+}
