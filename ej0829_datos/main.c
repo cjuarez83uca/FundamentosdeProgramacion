67107428b864519138f745884a10677008e58859
Carlos Juárez <cjuarez@uca.edu.sv>
2022-08-31 14:01:21 -0600

    ejercicio 0829

---
+++ ej0829_datos/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>  
+int main(int argc, char *argv){
+    // declarar variables
+    float cont;
+
+    printf("Introduzca un numero: ");
+    scanf("%f",&cont);
+
+    printf("El numero ingresado fue: %d",cont);
+
+return 0;
+}
