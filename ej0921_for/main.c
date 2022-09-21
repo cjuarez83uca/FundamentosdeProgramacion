0d2f25b1052e90552647099ffa542e5f50aba52e
cjuarez83uca <cjuarez@uca.edu.sv>
2022-09-21 14:29:49 -0600

    ejemplosfor

---
+++ ej0921_for/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(int argc,char *varg[]){
+    int x;
+    for(x=0;x<=9;x++) printf("%d\n",x);
+    return 0;
+}
---
+++ ej0921_formenos/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(int argc,char *varg[]){
+    int x;
+    for(x=9;x>=0;x-=2) printf("%d\n",x);
+    return 0;
+}
